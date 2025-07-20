988c3188433f09e15fde2b49b192958b9f174a2c
Shubham Adelkar <adelkarshubham47@gmail.com>
2025-07-20 14:06:22 +0530

    modify

--- src/root/user/pages/HomePage.jsx
+++ src/root/user/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 const HomePage = () => {
   const { logout } = useAuth();
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col bg-background">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4">
           <div className="min-h-screen flex items-center justify-center flex-1">
